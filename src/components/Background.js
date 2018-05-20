afa4d0bb8fa9b6224fa6a3e6a24881dd89f3bf38
Hunter Trujillo <cryptoquick@gmail.com>
2018-05-20 07:46:04 -0600

    Add H1. Add Made With GitCoin.

--- src/components/Background.js
+++ src/components/Background.js
@@ -8,5 +8,7 @@ export default ({isBlurred = false, children}) => (
   })}>
     {children}
     <img className="circle" src="/images/circle.png" width="784" height="795" role="presentation" />
+    <h1>An Avatar for<br />your <span className="h1-5">ETH</span> address</h1>
+    <img className="made-with-gitcoin" src="/images/made_with_gitcoin.png" width="400" height="67" alt="Made With GitCoin" />
   </div>
 );
--- src/components/NoWeb3.js
+++ src/components/NoWeb3.js
@@ -9,7 +9,7 @@ export default () => (
       <Header selectedIndex={0} />
     </Background>
     <main className="container centered">
-      <h1>No Connection To The Ethereum Network</h1>
+      <h2>No Connection To The Ethereum Network</h2>
       <p>Browse this website with:</p>
       <p>Metamask / Parity / Mist</p>
       <a href="/help">Need Help?</a>
